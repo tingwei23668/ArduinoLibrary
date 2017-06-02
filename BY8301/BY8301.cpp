9326c7c5f99c74f881c716bfdb68cd4f2dc56c2b
tingwei23668 <tingwei23668@163.com>
2017-06-02 10:00:35 +0800

    BY8301
    by8301

---
+++ BY8301/BY8301.cpp
@@ -0,0 +1,244 @@
+#include "BY8301.h"
+//#include"BY8301_def.h"
+#include <SoftwareSerial.h>
+BY8301::BY8301(SoftwareSerial *ser){
+	common_init();
+		audioSwSerial = ser;
+}
+BY8301::BY8301(HardwareSerial *ser){
+		common_init();
+		audioHwSerial = ser;
+}
+void BY8301::common_init(void){
+		audioSwSerial = NULL;
+		audioHwSerial = NULL;
+}
+void BY8301::begin(uint16_t _baud){
+	baud = _baud;
+	if(audioSwSerial){
+		audioSwSerial->begin(baud);
+	}else{
+		audioHwSerial->begin(baud);
+	}
+	delay(100);
+}
+/**
+	初始化操作
+**/
+void BY8301::init(uint8_t mode,uint8_t vol){
+	begin(9600);//调用本函数
+	//reset();复位
+	setMode(mode);
+	return volumn(vol);
+}
+void  BY8301::sendCommand(uint8_t cmd,uint8_t *buf,uint16_t len){
+	sendBuffer[0] = STX;
+	sendBuffer[1] = len+3;//长度，操作码，校验码
+	sendBuffer[2] = cmd;
+	sendBuffer[len+4] = ETX;
+	sendBuffer[len+3]=(sendBuffer[1]^cmd);
+	if(len>0){
+		memcpy(sendBuffer+3,buf,len);
+		for(int i=0;i<len;i++){
+			sendBuffer[len+3]^=sendBuffer[3+i];
+		}
+	}
+	if(audioSwSerial){
+		audioSwSerial->write(sendBuffer,len+5);
+	}else{
+		audioHwSerial->write(sendBuffer,len+5);
+	}
+	delay(160);
+	return sendBuffer;
+}
+/*控制指令*/
+
+void BY8301::play(){
+	sendCommand(CMD_PLAY,NULL,0);
+}
+void BY8301::pause(){
+	sendCommand(CMD_PAUSE,NULL,0);
+}
+void BY8301::next(){
+	sendCommand(CMD_NEXT,NULL,0);
+}
+void BY8301::prev(){
+	sendCommand(CMD_PREV,NULL,0);
+}
+void BY8301::volUp(){
+	sendCommand(CMD_UP,NULL,0);
+}
+void BY8301::volDown(){
+	sendCommand(CMD_DOWN,NULL,0);
+}
+void BY8301::sleep(){
+	sendCommand(CMD_SLEEP,NULL,0);
+}
+void BY8301::reset(){
+	sendCommand(CMD_RESET,NULL,0);
+	delay(1500);
+}
+void BY8301::fastf(){
+	sendCommand(CMD_FASTF,NULL,0);
+}
+void BY8301::fastd(){
+	sendCommand(CMD_FASTD,NULL,0);
+}
+void BY8301::stop(){
+	sendCommand(CMD_STOP,NULL,0);
+}
+void BY8301::volumn(uint8_t vol){
+	cmdBuffer[0] = vol;
+	sendCommand(CMD_VOL,cmdBuffer,1);
+}
+void BY8301::eq(uint8_t eq){
+	cmdBuffer[0] = eq;
+	sendCommand(CMD_EQ,NULL,0);
+}
+void BY8301::setMode(uint8_t mode){
+	cmdBuffer[0] = mode;
+	sendCommand(CMD_MODE,cmdBuffer,1);
+}
+void BY8301::folder(uint8_t temp ){//文件夹切换
+	cmdBuffer[0] = temp;
+	sendCommand(CMD_FOLDER,cmdBuffer,1);
+}
+void BY8301::setDevice(uint8_t device){
+	cmdBuffer[0] = device;
+	sendCommand(CMD_DEVICE,NULL,0);
+}
+void BY8301::choose(uint16_t num){
+	cmdBuffer[0] = num>>8;//参数高位，数组复制时为高位
+	cmdBuffer[1] = num&0xFF;
+	sendCommand(CMD_CHOOSE,cmdBuffer,2);
+}
+void BY8301::chooseFile(uint8_t folder, uint8_t file){
+	cmdBuffer[0] = file;
+	cmdBuffer[1] = folder;
+	sendCommand(CMD_FILECH,NULL,0);
+}
+/*查询指令*/
+uint16_t BY8301::queryNum(uint8_t cmd){
+	if(audioSwSerial){
+		audioSwSerial->stopListening();
+		audioSwSerial->listen();
+	}else{
+		audioHwSerial->end();
+		audioHwSerial->begin(baud);
+	}
+	sendCommand(cmd,NULL,0);
+	return dataParse();
+}
+
+uint16_t BY8301::dataParse(){
+	uint16_t sum=0;
+	uint8_t temp;
+	if(audioSwSerial){
+		while(audioSwSerial->available()){
+			temp = char(audioSwSerial->read());
+			if(temp>47&&temp<58){
+				temp-=48;
+				sum=sum*16+temp;
+			}else if(temp>96&&temp<103){
+				temp-=87;
+				sum=sum*16+temp;
+			}
+			
+			delay(1);
+		}	
+	}else{
+		while(audioHwSerial->available()){
+			temp = char(audioHwSerial->read());
+			if(temp>47&&temp<58){
+				temp-=48;
+				sum=sum*16+temp;
+			}else if(temp>96&&temp<103){
+				temp-=87;
+				sum=sum*16+temp;
+			}
+			delay(1);
+		}	
+		
+	}
+	return sum;
+	
+}
+uint16_t BY8301::querySTA(){
+	return queryNum(QUERY_STA);
+}
+uint16_t BY8301::queryVOL(){
+	return queryNum(QUERY_VOL);
+}
+uint16_t BY8301::queryEQ(){
+	return queryNum(QUERY_EQ);
+}
+uint16_t BY8301::queryMode(){
+	return queryNum(QUERY_MODE);
+}
+uint16_t BY8301::queryVersion(){
+	return queryNum(QUERY_VERSION);
+}
+uint16_t BY8301::queryUfile(){
+	return queryNum(QUERY_UFILE);
+}
+uint16_t BY8301::queryFlashFile(){
+	return queryNum(QUERY_FLASH_FILE);
+}
+uint16_t BY8301::queryDevice(){
+	return queryNum(QUERY_DEVICE);
+}
+uint16_t BY8301::queryUName(){
+	return queryNum(QUERY_UNAME);
+}
+uint16_t BY8301::queryFlashName(){
+	return queryNum(QUERY_FLASH_NAME);
+}
+uint16_t BY8301::queryPlayTime(){
+	return queryNum(QUERY_PTIME);
+}
+uint16_t BY8301::queryTotalTime(){
+	return queryNum(QUERY_TTIME);
+}
+uint16_t BY8301::queryFiles(){
+	return queryNum(QUERY_FILES);
+}
+//查询应读取串口数据<暂时查不到>
+//String queryName(){
+//	sendCommand(QUERY_NAME,NULL,0);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BY8301/BY8301.h
@@ -0,0 +1,146 @@
+﻿//BY8301 audio library for Arduino
+//VERSION 0.1.0
+#ifndef _BY8301_H_
+#define _BY8301_H_
+
+#if defined(ARDUINO) && ARDUINO >=100
+		#include <Arduino.h>
+#else
+		#include<WProgram.h>
+		#include<pins_arduino.h>
+#endif
+
+#include <SoftwareSerial.h>
+//#include "BY8301_def.h"
+#define STX 0x7E //起始码
+#define ETX 0xEF //结束码
+
+//操作码
+//通信控制指令
+#define CMD_PLAY    0x01    //播放
+#define CMD_PAUSE   0x02	//暂停
+#define CMD_NEXT    0x03	//下一曲
+#define CMD_PREV	0x04	//上一曲
+#define CMD_UP		0x05 	//音量加
+#define CMD_DOWN	0x06	//音量减
+#define CMD_SLEEP   0x07	//待机/正常工作（转换：待机下转为工作，工作下转为待机,待机电流：10mA）
+#define CMD_RESET   0x09	//复位
+#define	CMD_FASTF	0x0A	//快进-->FLASH无此功能
+#define CMD_FASTD	0x0B	//快退-->FLASH无此功能
+#define CMD_STOP	0x0E	//停止
+#define CMD_VOL		0x31	//设置音量:0-30级,掉电记忆
+#define CMD_EQ		0x32	//设置EQ-->均衡器：0-5(NO/POP/ROCK/JAZZ/CLASSIC/BASS)(掉电记忆)
+#define CMD_MODE    0x33	//设置循环模式：0-4(全盘/文件夹/单曲/随机/无循环)
+#define CMD_FOLDER  0x34	//文件夹切换:0-->上一个文件夹，1-->下一个文件夹
+#define CMD_DEVICE	0x35	//设备切换：0(U),1(FLASH)
+#define CMD_CHOOSE  0x41	//选择播放曲目：1-255首(掉电记忆)
+#define CMD_FILECH  0x42    //指定文件夹曲目播放
+
+//通信查询指令
+#define QUERY_STA   	0x10	//查询播放状态：0(停止)1(播放)2(暂停)3(快进)4(快退)
+#define QUERY_VOL   	0x11	//查询音量大小：1-30级(掉电记忆)
+#define QUERY_EQ    	0x12	//查询当前EQ：0-5(NO/POP/ROCK/JAZZ/CLASSIC/BASS)(掉电记忆)
+#define QUERY_MODE  	0x13	//查询当前播放模式：0-4(全盘/文件夹/单曲/随机/无循环)
+#define QUERY_VERSION 	0x14	//查询版本号：1.0
+#define QUERY_UFILE		0x16	//查询U盘总文件数：1-65535
+#define QUERY_FLASH_FILE	0x17	//查询FLASH的总文件数 1-255
+#define QUERY_DEVICE	0x18	//查询当前播放设备：0-->USB,2-->SPI
+#define QUERY_UNAME		0x1A	//查询U盘的当前曲目：1-65535
+#define	QUERY_FLASH_NAME	0x1B	//查询FLASH的当前曲目：1-255
+#define QUERY_PTIME 	0x1C	//查询当前播放歌曲的时间：返回时间(秒)
+#define QUERY_TTIME 	0x1D	//查询当前播放歌曲总时间：返回时间(秒)
+#define QUERY_NAME  	0x1E	//返回歌曲名(SPI内部无法返回)
+#define QUERY_FILES 	0x1F	//0-65536(SPI内部无法返回)
+
+class BY8301{
+public:
+	BY8301(SoftwareSerial *ser);
+	BY8301(HardwareSerial *ser);
+	void begin(uint16_t baud);
+	void init(uint8_t mode, uint8_t vol);//初始化是否设置模式？？？
+	
+	void play();
+	void pause();
+	void next();
+	void prev();
+	void volUp();
+	void volDown();
+	void sleep();
+	void reset();
+	void fastf();
+	void fastd();
+	void stop();
+	void volumn(uint8_t vol);
+	void eq(uint8_t eq);
+	void setMode(uint8_t temp);
+	void folder(uint8_t temp);
+	void setDevice(uint8_t device);
+	void choose(uint16_t num);
+	void chooseFile(uint8_t folder, uint8_t file);
+	
+	uint16_t querySTA();
+	uint16_t queryVOL();
+	uint16_t queryEQ();
+	uint16_t queryMode();
+	uint16_t queryVersion();
+	uint16_t queryUfile();
+	uint16_t queryFlashFile();
+	uint16_t queryDevice();
+	uint16_t queryUName();
+	uint16_t queryFlashName();
+	uint16_t queryPlayTime();
+	uint16_t queryTotalTime();
+	uint16_t queryFiles();
+	///String queryName();
+	
+private:
+	uint16_t baud;
+	uint8_t sendBuffer[8];
+	uint8_t cmdBuffer[8];
+
+	SoftwareSerial *audioSwSerial;
+	HardwareSerial *audioHwSerial;
+  
+	void common_init(void);
+	void sendCommand(uint8_t cmd, uint8_t *buf, uint16_t len);
+	uint16_t dataParse();
+	uint16_t queryNum(uint8_t cmd);
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
